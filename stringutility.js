2f83fddfcd0f4bbc011c8c591313d6faf57e7d21
Khushboo Priya <khushboopriyaiiitnr@gmail.com>
2021-02-19 02:15:12 +0530

    styling done

--- stringutility.js
+++ stringutility.js
@@ -4,6 +4,7 @@ const charcount = document.querySelector("#charactercount");
 const reverse = document.querySelector("#reverse");
 const wordcount = document.querySelector("#wordcount");
 const input = document.querySelector("#input-text");
+const clear = document.querySelector("#clear");
 
 lcase.addEventListener('click',()=>{
     document.querySelector("#output-text").value = input.value.toLowerCase();
@@ -19,4 +20,9 @@ reverse.addEventListener('click',()=> {
 });
 wordcount.addEventListener('click',()=> {
     document.querySelector("#output-text").value = parseInt(input.value.split(" ").length);
+});
+
+clear.addEventListener('click',()=>{
+    input.value = "";
+    document.querySelector("#output-text").value = "";
 });
